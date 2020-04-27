9acf97bafeebbd318a4dd0f8e7c4de61b1649a68
Joachim <jj@jj22.de>
2020-04-27 14:34:14 +0000

    done

--- src/de/uulm/in/pm/test/Main.java
+++ src/de/uulm/in/pm/test/Main.java
@@ -3,7 +3,6 @@ package de.uulm.in.pm.test;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.uulm.in.pm.api.AbstractBird;
 import de.uulm.in.pm.api.AnimalIsDeadException;
 import de.uulm.in.pm.api.Eagle;
 
@@ -19,6 +18,7 @@ public class Main {
 		System.out.println("amos == amos2 | " + (amos == amos2));
 		System.out.println("amos.equals(amos2) | " + amos.equals(amos2));
 		try {
+			@SuppressWarnings("unused")
 			Eagle noBird = new Eagle(-1, 2, "Harald");
 		} catch (IllegalArgumentException e) {
 			// System.out.println(e);
